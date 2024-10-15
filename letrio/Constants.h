bb2f8fb81f657f0b53770973fdf40800b44ad25c
Myles Rockland <myles.rockland@students.plymouth.ac.uk>
2024-10-15 21:57:47 +0100

    Add Constants.h

---
+++ letrio/Constants.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+const int WINDOW_WIDTH = 600;
+const int WINDOW_HEIGHT = 400;
+const int FONT_SIZE = 72;
+const int GRID_WIDTH = 10;
+const int GRID_HEIGHT = 18;
+const int START_SPEED = 1100; // Inversely proportional to level
+const int START_LEVEL = 1; // Might need to be 0.
+const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
