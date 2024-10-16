1b680d86c84b22c39ef6751a86d64c525e8998e2
Myles Rockland <myles.rockland@students.plymouth.ac.uk>
2024-10-16 23:09:06 +0100

    Add constants

--- letrio/Constants.h
+++ letrio/Constants.h
@@ -3,9 +3,13 @@
 
 const int WINDOW_WIDTH = 600;
 const int WINDOW_HEIGHT = 400;
-const int FONT_SIZE = 72;
 const int GRID_WIDTH = 10;
 const int GRID_HEIGHT = 18;
-const int START_SPEED = 1100; // Inversely proportional to level
+const int CELL_LENGTH = WINDOW_HEIGHT / GRID_HEIGHT;
+const int FONT_SIZE = CELL_LENGTH;
+const int START_SPEED = 1000; // Inversely proportional to level
 const int START_LEVEL = 1; // Might need to be 0.
-const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const int MIN_WORD_SIZE = 4;
+const int PALINDROME_MULTIPLIER = 2;
+const std::string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const std::string WORDS_FILE_PATH = "words_alpha.txt";
